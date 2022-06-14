294ff735d809c58e5f96e245af6f8bd47bf81258
Ayoub Ghozzi <ghozziayoub@gmail.com>
2022-06-14 12:11:18 +0100

    create somepublic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/public/home/home.component';
+import { AboutComponent } from './components/public/about/about.component';
+import { CoursesComponent } from './components/public/courses/courses.component';
+import { OurTeamComponent } from './components/public/our-team/our-team.component';
+import { ContactComponent } from './components/public/contact/contact.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { RegisterComponent } from './components/public/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    CoursesComponent,
+    OurTeamComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
