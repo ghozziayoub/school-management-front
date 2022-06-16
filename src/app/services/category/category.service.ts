f297db29f7e9c556b2bcbcb1a4b17772a1e80483
omarl68 <omar.lab68@gmail.com>
2022-06-16 12:00:41 +0100

    organize architecture-2

---
+++ src/app/services/category/category.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Category } from '../../models/category';
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private getAllCategoriesUrl = "http://localhost:3000/categories";
+  private getOneCategoryUrl = "http://localhost:3000/categories/";
+  private deleteCategoryUrl = "http://localhost:3000/categories/";
+  private addCategoryUrl = "http://localhost:3000/categories/";
+  private updateCategoryUrl = "http://localhost:3000/categories/";
+
+  constructor(private http: HttpClient) { }
+
+  getAllCategories() {
+    return this.http.get<any>(this.getAllCategoriesUrl);
+  }
+
+  getOneCategory(id: String) {
+    return this.http.get<any>(this.getOneCategoryUrl + id)
+  }
+
+  deleteCategory(id: String) {
+    return this.http.delete<any>(this.deleteCategoryUrl + id)
+  }
+
+  addCategory(Category: Category) {
+    return this.http.post<any>(this.addCategoryUrl, Category);
+  }
+
+  updateCategory(Category: Category) {
+    return this.http.put<any>(this.updateCategoryUrl, Category);
+  }
+}
---
+++ src/app/services/training/training.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Training } from '../../models/training';
+@Injectable({
+  providedIn: 'root'
+})
+export class TrainingService {
+
+ 
+  private getAllTrainingsUrl = "http://localhost:3000/trainings";
+  private getOneTrainingUrl = "http://localhost:3000/trainings/";
+  private deleteTrainingUrl = "http://localhost:3000/trainings/";
+  private addTrainingUrl = "http://localhost:3000/trainings/";
+  private updateTrainingUrl = "http://localhost:3000/trainings/";
+
+  constructor(private http: HttpClient) { }
+
+  getAllTrainings() {
+    return this.http.get<any>(this.getAllTrainingsUrl);
+  }
+
+  getOneTraining(id: String) {
+    return this.http.get<any>(this.getOneTrainingUrl + id)
+  }
+
+  deleteTraining(id: String) {
+    return this.http.delete<any>(this.deleteTrainingUrl + id)
+  }
+
+  addTraining(Training: Training) {
+    return this.http.post<any>(this.addTrainingUrl, Training);
+  }
+
+  updateTraining(Training: Training) {
+    return this.http.put<any>(this.updateTrainingUrl, Training);
+  }
+}
