6dbea8073b41df3bfc2bd978f750d04295f9be66
omarl68 <omar.lab68@gmail.com>
2022-06-30 14:43:26 +0100

    features and updates attributs

--- src/app/components/private/shared/trainers/add-trainer/add-trainer.component.ts
+++ src/app/components/private/shared/trainers/add-trainer/add-trainer.component.ts
@@ -7,32 +7,65 @@ import {
 } from '@angular/forms';
 import { TrainerService } from '../../../../../services/trainer.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
+contactForm: FormGroup
 @Component({
   selector: 'app-add-trainer',
   templateUrl: './add-trainer.component.html',
   styleUrls: ['./add-trainer.component.scss'],
 })
+
 export class AddTrainerComponent implements OnInit {
+
   myForm: FormGroup;
   selectedFile: any;
   imageUrl = 'assets/img/avatar.png';
+  numberRegEx = /\-?\d*\.?\d{1,2}/;
+
 
   constructor(
     private fb: FormBuilder,
     private trainerService: TrainerService,
-    private router: Router
+    private router: Router,private toastr:ToastrService
   ) {
     this.myForm = this.fb.group({
-      firstname: new FormControl(),
-      lastname: new FormControl(),
-      email: new FormControl(),
-      speciality: new FormControl(),
-      yearsOfExperience: new FormControl(),
+      firstname: new FormControl('', [
+        Validators.required,
+        Validators.pattern("[A-Za-z .'-]+"),
+        Validators.minLength(2)
+      ]),
+      lastname: new FormControl('', [
+        Validators.required,
+        Validators.pattern("[A-Za-z .'-]+"),
+        Validators.minLength(2)
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      speciality: new FormControl('', [
+        Validators.required,
+        Validators.pattern("[A-Za-z .'-]+"),
+        Validators.minLength(2)
+      ]),
+      yearsOfExperience: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.numberRegEx),
+        Validators.minLength(1)
+      ]),
+      facebook: new FormControl(),
+      instagram: new FormControl(),
+      twitter: new FormControl(),
     });
   }
+  get firstname() { return this.myForm.get('firstname') }
+  get lastname() { return this.myForm.get('lastname') }
+  get email() { return this.myForm.get('email') }
+  get speciality() { return this.myForm.get('speciality') }
+  get yearsOfExperience() { return this.myForm.get('yearsOfExperience') }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   save(event: any) {
     let reader = new FileReader();
@@ -49,17 +82,23 @@ export class AddTrainerComponent implements OnInit {
 
   add() {
     let data = this.myForm.value;
+
     let formData = new FormData();
-    formData.append('firstname', data.firstname),
-      formData.append('lastname', data.lastname),
-      formData.append('email', data.email),
-      formData.append('speciality', data.speciality),
-      formData.append('yearsOfExperience', data.yearsOfExperience),
-      formData.append('picture', this.selectedFile);
-    console.log(this.selectedFile);
+
+    formData.append('firstname', data.firstname)
+    formData.append('lastname', data.lastname)
+    formData.append('email', data.email)
+    formData.append('speciality', data.speciality)
+    formData.append('yearsOfExperience', data.yearsOfExperience)
+    formData.append('facebook', data.facebook)
+    formData.append('instagram', data.instagram)
+    formData.append('twitter', data.twitter)
+    formData.append('picture', this.selectedFile);
+
     this.trainerService.addTrainer(formData).subscribe({
       next: (result) => {
         console.log(result);
+        this.toastr.success('saved successfully');
         this.router.navigate(['/trainer-list']);
       },
       error: (err) => {
