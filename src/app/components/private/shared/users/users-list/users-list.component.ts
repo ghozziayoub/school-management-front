454350f1a2eaf88b885a8a15c7686fba103d636c
Likrania <kraiemkhalil35@gmail.com>
2022-06-30 12:51:27 +0100

    users are affiched in usersmanagment

--- src/app/components/private/shared/users/users-list/users-list.component.ts
+++ src/app/components/private/shared/users/users-list/users-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users-list',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent implements OnInit {
+userList:any[]=[]
 
-  constructor() { }
+
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
+    this.userService.getAllUsers().subscribe({
+      next: (result) =>{
+        this.userList=result
+      }
+    })
+  }
+  delete(id: string, index: number) {
+    this.userList.splice(index, 1);
+
+    this.userService.deleteUser(id).subscribe({
+      next: (result) => {
+        console.log(result);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
 }
