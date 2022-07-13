59d97bea2765b9a3a2efc92de2185eaa0376a1ae
ahmed <ahmedachour789@gmail.com>
2022-07-13 11:58:59 +0100

    patch: change the single article page

--- src/app/components/private/shared/blog/single-article/single-article.component.ts
+++ src/app/components/private/shared/blog/single-article/single-article.component.ts
@@ -1,59 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  FormBuilder,
-} from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BlogService } from '../../../../../services/blog.service';
 import { BaseService } from 'src/app/services/base.service';
 @Component({
   selector: 'app-single-article',
   templateUrl: './single-article.component.html',
-  styleUrls: ['./single-article.component.scss']
+  styleUrls: ['./single-article.component.scss'],
 })
 export class SingleArticleComponent implements OnInit {
-
-  myForm: FormGroup;
-  user: any;
-  userList: any[] = [];
-  selectedFile: any;
+  article: any;
   imageUrl = `${BaseService.baseUrl}/`;
-
-  constructor(
-    private fb: FormBuilder,
-    private blogService: BlogService,
-    private router: Router,
-    private route: ActivatedRoute,
-  ) {
-    let article = {
-      titre: new FormControl(),
-      content: new FormControl(),
-    };
-    this.myForm = this.fb.group(article);
-  }
-  get titre() {
-    return this.myForm.get('titre');
-  }
-  get content() {
-    return this.myForm.get('content');
-  }
+  constructor(private route:ActivatedRoute, private blogService:BlogService, private router:Router) {}
 
   ngOnInit(): void {
     let id = this.route.snapshot.params?.['id'];
     this.blogService.getOneArticles(id).subscribe({
-      next: (result) => {
-        let article = result;
-        this.myForm.patchValue({
-          titre: article.titre,
-          content: article.content,
-        });
-        this.imageUrl += article.image;
+      next: (res)=>{
+        this.article = res
       },
-      error: (error) => {
-        console.log(error);
-      },
-    });
-  }
+      error: (err)=>{
+        console.log(err),
+        this.router.navigate(['/404page'])
+      }
+    })
 
+  }
 }
