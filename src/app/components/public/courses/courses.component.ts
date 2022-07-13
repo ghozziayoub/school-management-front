e1adc8cacb9093b396226a80f00564f6219bbb4e
ahmed <ahmedachour789@gmail.com>
2022-07-13 14:33:19 +0100

    patch: remove image and filter the courses

--- src/app/components/public/courses/courses.component.ts
+++ src/app/components/public/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 import { BaseService } from 'src/app/services/base.service';
 import { CategoryService } from 'src/app/services/category.service';
 import { TrainingService } from 'src/app/services/training.service';
@@ -10,17 +11,17 @@ import { TrainingService } from 'src/app/services/training.service';
 export class CoursesComponent implements OnInit {
   categoriesList: any = [];
   trainingsList: any = [];
+  allTrainingsList: any = [];
   baseUrl = `${BaseService.baseUrl}/`;
   constructor(
     private categoryService: CategoryService,
     private trainingService: TrainingService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.categoryService.getAllCategories().subscribe({
       next: (res) => {
         this.categoriesList = res;
-        console.log(res);
       },
       error: (err) => {
         console.log(err);
@@ -30,11 +31,37 @@ export class CoursesComponent implements OnInit {
     this.trainingService.getAllTrainings().subscribe({
       next: (res) => {
         this.trainingsList = res;
-        console.log(res);
+        this.allTrainingsList = res;
       },
       error: (err) => {
         console.log(err);
       },
     });
+  }
+
+  filter(name: String) {
+    if (name != 'all') {
+      this.trainingsList = this.allTrainingsList.filter(
+        (training: any) => training.category.name == name
+      );
+    } else {
+      this.trainingsList = this.allTrainingsList;
+    }
+  }
+
+  onclick($event: any) {
+    console.log($event);
+    let clickedElement = $event.target || $event.srcElement;
+    console.log(clickedElement.nodeName);
+    if (clickedElement.nodeName === 'DIV') {
+      let isCertainButtonAlreadyActive =
+        clickedElement.parentElement.querySelector('.active');
+      // if a Button already has Class: .active
+      if (isCertainButtonAlreadyActive) {
+        isCertainButtonAlreadyActive.classList.remove('active');
+      }
+
+      clickedElement.className += ' active';
+    }
   }
 }
